a413808595a6f6c87909efbed2f7ea6a9aaebcda
ibrahim ahmedoglu <ibrahimahmedoglu@ibrahims-MacBook-Pro.local>
2022-06-28 18:54:32 +0300

    Added my project

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,30 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by ibrahim ahmedoglu on 18.06.2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+}
